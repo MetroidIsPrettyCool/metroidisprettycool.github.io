a214bb075bc17427523cd3f1784edd326e2d1051
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2025-10-20 17:39:24 -0700

    update site
    Restructured to separate source and html, set up org-publish,
    added a projects entry for my balanced ternary full adder

---
+++ .dir-locals.el
@@ -0,0 +1,25 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 120)))
+ (org-mode . ((eval . (load-file (concat (car (dir-locals-find-file ".")) ".emacs/yt-embed.el")))))
+ (org-mode . ((eval . (setq-local org-publish-project-alist `(("metroidisprettycool.github.io"
+                                                               :base-directory
+                                                               ,(concat (car (dir-locals-find-file "."))
+                                                                        "src/")
+
+                                                               :recursive
+                                                               t
+
+                                                               :publishing-function
+                                                               org-html-publish-to-html
+
+                                                               :publishing-directory
+                                                               ,(concat (car (dir-locals-find-file "."))
+                                                                        "docs/")
+
+                                                               :html-link-home
+                                                               "/index.html"
+
+                                                               :html-link-up
+                                                               "../index.html")))))))
---
+++ .emacs/yt-embed.el
@@ -0,0 +1,30 @@
+;;; yt-embed.el --- org-mode links to embed YouTube videos  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; THIS CODE IS NOT MY WORK! COPIED FROM
+;; <https://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html>
+
+;;; Code:
+
+(setq-local yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe"
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
+;;; yt-embed.el ends here
