a56c2decade3ebda99c4772bf463146674d2a1ea
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2025-10-30 13:02:30 -0700

    smaller update

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,32 +2,25 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((fill-column . 120)))
- (org-mode . ((eval . (load-file (concat (car (dir-locals-find-file ".")) ".emacs/yt-embed.el")))))
- (org-mode . ((eval . (setq-local org-publish-project-alist `(("metroidisprettycool.github.io"
-                                                               :base-directory
-                                                               ,(concat (car (dir-locals-find-file "."))
-                                                                        "src/")
+ (org-mode . ((eval . (require 'yt-embed (concat (project-root (project-current)) ".emacs/yt-embed.el")))))
+ (org-mode
+  .
+  ((eval .
+         (setq-local
+          org-publish-project-alist
+          `(("metroidisprettycool.github.io"
+             :base-directory ,(concat (project-root (project-current)) "src/")
 
-                                                               :recursive
-                                                               t
+             :recursive t
 
-                                                               :publishing-function
-                                                               org-html-publish-to-html
+             :publishing-function org-html-publish-to-html
 
-                                                               :publishing-directory
-                                                               ,(concat (car (dir-locals-find-file "."))
-                                                                        "docs/")
+             :publishing-directory ,(concat (project-root (project-current)) "docs/")
 
-                                                               :html-link-home
-                                                               "/index.html"
+             :html-link-home "/index.html"
 
-                                                               :html-link-up
-                                                               "./index.html"
+             :html-link-up "./index.html"
 
-                                                               :html-doctype
-                                                               "xhtml-frameset"
+             :html-doctype "xhtml-frameset"
 
-                                                               :html-head-extra
-                                                               ,(concat "<link rel=\"icon\""
-                                                                        " type=\"image/png\""
-                                                                        " href=\"/favicon.ico\" />"))))))))
+             :html-head-extra "<link rel=\"icon\" type=\"image/png\" href=\"/favicon.ico\" />")))))))
--- .emacs/yt-embed.el
+++ .emacs/yt-embed.el
@@ -2,30 +2,29 @@
 
 ;;; Commentary:
 
-;; THIS CODE IS NOT MY WORK! COPIED FROM
-;; <https://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html>
+;; ADAPTED FROM <https://endlessparentheses.com/embedding-youtube-videos-with-org-mode-links.html>
 
 ;;; Code:
 
-(defvar yt-iframe-format
-  ;; You may want to change your width and height.
-  (concat "<iframe width=\"600\""
-          " height=\"338\""
-          " src=\"https://www.youtube.com/embed/%s\""
-          " frameborder=\"0\""
-          ">%s</iframe>"))
+(require 'ol)
 
-(org-add-link-type
- "yt"
- (lambda (handle)
-   (browse-url
-    (concat "https://www.youtube.com/embed/"
-            handle)))
- (lambda (path desc backend)
-   (cl-case backend
-     (html (format yt-iframe-format
-                   path (or desc "")))
-     (latex (format "\href{%s}{%s}"
-                    path (or desc "video"))))))
+(let ((yt-iframe-format
+       (concat
+        "<iframe width=\"600\""
+        " height=\"338\""
+        " src=\"https://www.youtube.com/embed/%s\""
+        " frameborder=\"0\""
+        ">%s</iframe>")))
+  (org-link-set-parameters
+   "yt"
+   :follow (lambda (path _) (browse-url-xdg-open (concat "https://www.youtube.com/embed/" path)))
+   :export
+   (lambda (path desc backend _)
+     (pcase backend
+       (`html (format yt-iframe-format path (or desc "")))
+       (`latex (format "\href{%s}{%s}" path (or desc "video")))
+       (_ path)))))
+
+(provide 'yt-embed)
 
 ;;; yt-embed.el ends here
