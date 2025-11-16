6165e351101daff1cafee6002f2fcf361395fdf0
HaHaIHaveAShare <hahaihaveashare@gmail.com>
2025-11-16 01:28:50 -0800

    Update 5
    Significant engineering improvements; cleanup and revision of content.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,31 +2,34 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((fill-column . 120)))
- (org-mode . ((eval . (require 'metroidisprettycool-github-io
-                               (expand-file-name "metroidisprettycool-github-io.el" (project-root (project-current)))))
-              (org-export-global-macros . (("citation-footer" . "(eval (format \"* Citations:
-:PROPERTIES:
-:UNNUMBERED: notoc
-:END:
+ (org-mode . ((eval . (setq-local
+                       org-publish-project-alist
+                       `(("metroidisprettycool.github.io"
+                          :base-directory ,(expand-file-name "src/" (project-root (project-current)))
+                          :recursive t
+                          :exclude "common/"
 
-#+CITE_EXPORT: csl %ssrc/3rdparty/apa.csl
-#+PRINT_BIBLIOGRAPHY:
+                          :publishing-function org-html-publish-to-html
+                          :publishing-directory ,(expand-file-name "docs/" (project-root (project-current)))
 
------
+                          :with-entities     t
+                          :with-smart-quotes t
+                          :html-extension "xhtml"
 
-These formatted citations were generated with the
-[[https://github.com/citation-style-language/styles/blob/master/apa.csl][APA CSL style]] from the Citation Style
-Language Project, licensed under the [[https://creativecommons.org/licenses/by-sa/3.0/][Creative Commons
-Attribution-ShareAlike 3.0 Unported license]] 🅭🅯🄎.
-
-Learn more about the CSL project at their home page: [[https://citationstyles.org/]].
-\" (project-root (project-current t))))")
-                                           ("license-footer" . "* License:
-:PROPERTIES:
-:UNNUMBERED: notoc
-:END:
-
-Copyright $1 [[https://github.com/MetroidIsPrettyCool][Joseph Burke]]. All original content published to this website is
-licensed under a [[https://creativecommons.org/licenses/by-sa/4.0/][CC BY-SA 4.0]] 🅭🅯🄎 license, unless explicitly stated
-otherwise.
-"))))))
+                          :completion-function
+                          ,(lambda (project-plist)
+                             "Check for errors in exported HTML with tidy."
+                             (let* ((publishing-directory (plist-get project-plist :publishing-directory))
+                                    (extension (or (plist-get project-plist :html-extension) org-html-extension))
+                                    (published-files (if (plist-get project-plist :recursive)
+                                                         (directory-files-recursively publishing-directory
+                                                                                      (rx (literal extension) eos))
+                                                       (directory-files t publishing-directory
+                                                                        (rx (literal extension) eos))))
+                                    (tidy-args (append '("--gnu-emacs" "yes" "--markup" "no" "--show-filename" "yes")
+                                                       published-files)))
+                               (let ((tidy-result-buffer (compilation-start
+                                                          (mapconcat #'shell-quote-argument (cons "tidy" tidy-args) " ")
+                                                          'compilation-mode
+                                                          (lambda (mode-name) "*tidy-results*"))))
+                                 (run-at-time 0.1 nil (lambda () (display-buffer tidy-result-buffer)))))))))))))
